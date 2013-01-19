5bfe0c1c8c33da11bc9c748aa7c17bc398c94cf7
Miguel Alvarez <miguelazpeitia@yahoo.com>
2013-01-18 21:17:32 -0600

    83

---
+++ js/API/files.js
@@ -0,0 +1,66 @@
+// JavaScript Document
+$(document).ready(function(e) {
+    document.addEventListener("deviceready", function(){
+		$('#archivos .rounded li').tap(function(){
+			switch($(this).index()){
+				case 0://Escribir (Crear) Archivos
+					window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+					
+					function gotFS(fileSystem) {
+        fileSystem.root.getFile("readme.txt", {create: true, exclusive: false}, gotFileEntry, fail);
+    }
+
+    function gotFileEntry(fileEntry) {
+        fileEntry.createWriter(gotFileWriter, fail);
+    }
+
+    function gotFileWriter(writer) {
+        writer.onwriteend = function(evt) {
+           alert("EL archivo se escribio"); 
+           
+           
+        };
+        writer.write($("#archivosCamp").val());
+    
+	
+	
+	}
+
+										
+					break;
+				case 1://Leer Archivo
+
+		
+		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+		
+				function gotFS(fileSystem) {
+        fileSystem.root.getFile("readme.txt", null, gotFileEntry, fail);
+    }
+
+    function gotFileEntry(fileEntry) {
+        fileEntry.file(gotFile, fail);
+    }
+
+    function gotFile(file){
+        
+        readAsText(file);
+    }
+
+    
+    function readAsText(file) {
+        var reader = new FileReader();
+        reader.onloadend = function(evt) {
+			alert("Archivo leido");
+            //console.log(evt.target.result);
+        };
+        $("#archivosCamp").val(reader.readAsText(file));
+    }
+					
+			}
+		});
+	});
+});
+
+			function fail(err){
+				alert("error: "+err.code);
+				}
